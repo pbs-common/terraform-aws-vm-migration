58daa42f15eef2ccd1dbbc78295e4e8b98f0a540
Wilveren <w.mclean1986@gmail.com>
2026-09-18 14:46:49 -0400

    Changing approach to make incremental and full plans

--- environments/ad/main.tf
+++ environments/ad/main.tf
@@ -223,10 +223,10 @@ module "backup" {
   vault_name  = var.backup_vault_name
   kms_key_arn = var.kms_key_arn
 
-  incremental_schedule      = var.incremental_schedule
+  incremental_schedule       = var.incremental_schedule
   incremental_retention_days = var.incremental_retention_days
 
-  full_schedule      = var.full_schedule
+  full_schedule       = var.full_schedule
   full_retention_days = var.full_retention_days
 
   tags = var.tags
